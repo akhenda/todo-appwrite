c9fff025eb43d8c4b6c862f1d319c54c794e0f80
akhenda <akhenda@gmail.com>
2024-06-02 20:25:54 +0300

    chore: ✨ initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['plugin:@hendacorp/reactful'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    camelcase: 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    'no-process-env': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@shopify/strict-component-boundaries': 'off',
+    '@typescript-eslint/no-this-alias': ['error', { allowedNames: ['self'] }],
+    'consistent-this': ['error', 'self'],
+    '@typescript-eslint/member-ordering': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    'n/no-unpublished-import': 'off',
+  },
+};
